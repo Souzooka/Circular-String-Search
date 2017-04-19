730736de3ebde83e352b9f692827f53b747d6b6a
Dakota <bazookaharwell@gmail.com>
2017-04-19 09:29:45 -1000

    Stub out the application and write more rigorous tests

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+  * Note: While repeating the first string until the string
+  * is over the length of the second string + the length of the first string
+  * may be a valid solution, I decided to iterate over the strings
+  * like arrays to emulate the string being truly circular.
+  */
+
+/**
+  * Func circularStringSearch
+  * Parameters: String circularStr, String searchStr
+  * Return values:
+  * True -- if searchStr can be found in a circular circularStr.
+  * False -- Any other cases.
+  */
+
+function circularStringSearch(circularStr, searchStr) {
+  return true;
+}
+module.exports = circularStringSearch;
--- test/app_spec.js
+++ test/app_spec.js
@@ -7,13 +7,16 @@ describe('Circular String Search', function() {
 
   it('should find a smaller string located inside the first string', function() {
     expect(search('hotdog', 'dog')).to.be.equal(true);
+    expect(search('hotdog', 'cat')).to.be.equal(false);
   });
 
   it('should find a string that would exist if the first string was once-circular', function() {
     expect(search('hotdog', 'doghot')).to.be.equal(true);
+    expect(search('hotdog', 'doghat')).to.be.equal(false);
   });
 
   it('should find a string that would exist if the first string was infinitly circular', function() {
     expect(search('hotdog', 'doghotdoghot')).to.be.equal(true);
+    expect(search('hotdog', 'doghotdoghat')).to.be.equal(false);
   });
 });
